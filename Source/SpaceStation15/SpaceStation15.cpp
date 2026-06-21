4b2cd8ad0ce9108d5c6d40735d068e667421a86a
CrazyPhantom <GITHUB_NOREPLY_EMAIL>
2026-06-22 04:21:19 +1000

    Revert project

--- Source/SpaceStation15/SpaceStation15.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SpaceStation15.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceStation15, "SpaceStation15" );
-
-DEFINE_LOG_CATEGORY(LogSpaceStation15)
--- Source/SpaceStation15/SpaceStation15GameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SpaceStation15GameMode.h"
-
-ASpaceStation15GameMode::ASpaceStation15GameMode()
-{
-	// stub
-}
